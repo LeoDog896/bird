293c2f1e331a617dfa270327275451e06f2a701d
Tristan F <LeoDog896@hotmail.com>
2023-01-13 15:35:40 +0000

    feat: subscriber notification

--- index.ts
+++ index.ts
@@ -2,21 +2,27 @@
 interface Stream<T> {
   get(): T;
   set(input: T): void;
-  subscribers: ((value: T) => void)[];
+  subscribers: Subscriber<T>[];
 }
+
+type Subscriber<T> = (value: T) => void;
 
 export function stream<T>(initialValue: T): Stream<T>;
 export function stream<T>(): Stream<T | undefined>;
 export function stream<T>(initialValue?: T): Stream<T | undefined> {
   let value = initialValue;
+  const subscribers: Subscriber<T | undefined>[] = []
 
   return {
     set(input: T) {
       value = input;
+      for (const subscriber of subscribers) {
+        subscriber(value)
+      }
     },
     get() {
       return value;
     },
-    subscribers: [],
+    subscribers,
   };
 }
